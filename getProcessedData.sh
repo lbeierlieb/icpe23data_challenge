fe6dd089ef4cc3fc33652503e8bfcf3eebccddfb
Lukas Beierlieb <lukas.beierlieb@uni-wuerzburg.de>
2022-12-21 23:15:38 +0100

    Add scripts to retrieve raw and processed data

---
+++ getProcessedData.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Cloning data challenge repository"
+git clone https://github.com/SEALABQualityGroup/icpe-data-challenge-jmh.git
---
+++ getRawData.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#echo "Downloading archive with all raw files"
+#wget --verbose --output-document=jmh.tar.gz https://zenodo.org/record/5961018/files/jmh.tar.gz?download=1
+#
+#echo "Extracting"
+#tar -xvf jmh.tar.gz
+#mv data/jmh jmh
+#rmdir data
+
+echo "Removing failed benchmarks"
+for FILE in $(cat invalid_raw_files)
+do
+    rm --verbose "jmh/"$FILE
+done
